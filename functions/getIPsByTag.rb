2efb0c9f38ecefc5442ea8a887f6f048c8359f68
jloope <jloope@janrain.com>
2011-12-10 11:46:30 -0800

    whitespace changes and empty result check

--- functions/getIPsByTag.rb
+++ functions/getIPsByTag.rb
@@ -2,23 +2,27 @@ require 'rubygems'
 require 'fog'
 
 module Puppet::Parser::Functions
-        newfunction(:getIPsByTag, :type => :rvalue) do |args|
-                filter = { 'key' => args[0] }
-                filter = { 'value' => args[1] }
-                instances  = {}
+  newfunction(:getIPsByTag, :type => :rvalue) do |args|
+    filter = { 'key' => args[0] }
+    filter = { 'value' => args[1] }
+    instances  = {}
 
-                Fog.credentials_path = '/etc/puppet/fog_cred'
+    Fog.credentials_path = '/etc/puppet/fog_cred'
 
-                # new compute provider from AWS
-                compute = Fog::Compute.new(:provider => 'AWS')
+    # new compute provider from AWS
+    compute = Fog::Compute.new(:provider => 'AWS')
 
-                # print resource id's by the hash {key => value}
-                compute.describe_tags(filter).body['tagSet'].each do|tag|
-                        instance_facts = compute.describe_instances('instance-id' => tag['resourceId']).body['reservationSet'][0]['instancesSet'][0]
-                        instances[tag['resourceId']] = instance_facts['privateIpAddress']
-                end
+    # print resource id's by the hash {key => value}
+    compute.describe_tags(filter).body['tagSet'].each do|tag|
+      instance_facts = compute.describe_instances('instance-id' => tag['resourceId']).body['reservationSet'][0]['instancesSet'][0]
+      instances[tag['resourceId']] = instance_facts['privateIpAddress']
+    end
 
-                return instances
+    #An empty result causes a parse error, for safety sake
+    raise Puppet::ParseError, "Result is empty for key:" + filter['key'] +
+      " value: " + filter['value'] if instances.empty?
 
-        end
+    return instances
+
+  end
 end
--- functions/s3getEtag.rb
+++ functions/s3getEtag.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'fog'
 
 module Puppet::Parser::Functions
-        newfunction(:s3getcurl, :type => :rvalue) do |args|
-                bucket   = args[0]
-                key      = args[1]
-                Fog.credentials_path = '/etc/puppet/fog_cred'
-                s3 = Fog::Storage.new(:provider => 'AWS')
-                s3bucket = s3.directories.get(bucket)
-                etag = s3bucket.files.head(key).etag
-                return etag
-        end
+  newfunction(:s3getEtag, :type => :rvalue) do |args|
+    bucket   = args[0]
+    key      = args[1]
+    Fog.credentials_path = '/etc/puppet/fog_cred'
+    s3 = Fog::Storage.new(:provider => 'AWS')
+    s3bucket = s3.directories.get(bucket)
+    etag = s3bucket.files.head(key).etag
+    return etag
+  end
 end

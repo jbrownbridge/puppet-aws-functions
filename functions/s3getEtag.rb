ffc7ffd1bdd205945eff7dc1b92c881c1dcd8f89
jloope <jloope@janrain.com>
2012-01-24 15:41:30 -0800

    raise errors

--- functions/s3getEtag.rb
+++ functions/s3getEtag.rb
@@ -8,7 +8,13 @@ module Puppet::Parser::Functions
     Fog.credentials_path = '/etc/puppet/fog_cred'
     s3 = Fog::Storage.new(:provider => 'AWS')
     s3bucket = s3.directories.get(bucket)
+    if s3bucket.files.head(key).nil? then
+      raise Puppet::ParseError, "s3 object does not exist: #{key}"
+    end
     etag = s3bucket.files.head(key).etag
+    if etag.match(/-/) then
+      raise Puppet::ParseError, "md5 etag is broken, Don't upload with Multipart."
+    end
     return etag
   end
 end
